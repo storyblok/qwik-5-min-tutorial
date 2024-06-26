3b4fa1a26b6726aeaa696b14e34ec6cc96d0686f
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-06-26 15:29:16 +0530

    feat: added reusable useStoryblok hook
    This also fix the richtext resolver issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,14 @@
-import { $, component$, useSignal, useOnWindow } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { routeLoader$ } from "@builder.io/qwik-city";
+import type { ISbStoryData } from "@storyblok/js";
 
 // [Start] Define dynamic components (Part 1)
 import StoryblokComponent from "~/components/storyblok/component";
 // [End]
 
 // [Start] Storyblok Initial Setup
-import type { ISbStoryData } from "@storyblok/js";
-import { loadStoryblokBridge } from "@storyblok/js";
-import { storyblokApi } from "~/routes/plugin@storyblok";
+import { storyblokApi, useStoryblok } from "~/routes/plugin@storyblok";
 
 export const useStory = routeLoader$(async () => {
   if (!storyblokApi)
@@ -23,32 +22,9 @@ export const useStory = routeLoader$(async () => {
 // [End]
 
 export default component$(() => {
-  const story = useSignal(useStory().value);
+  const story = useStoryblok(useStory().value);
 
-  useOnWindow(
-    "load",
-    $(async () => {
-      await loadStoryblokBridge();
-      const { StoryblokBridge, location } = window;
-      const storyblokInstance = new StoryblokBridge();
-
-      storyblokInstance.on(["published", "change"], () => {
-        // reload page if save or publish is clicked
-        location.reload();
-      });
-
-      storyblokInstance.on("input", (event) => {
-        // Access currently changed but not yet saved content via:
-        story.value = event?.story as ISbStoryData;
-      });
-    })
-  );
-
-  return (
-    <>
-      <StoryblokComponent key={story.value.id} blok={story.value.content} />
-    </>
-  );
+  return <StoryblokComponent key={story.value.id} blok={story.value.content} />;
 });
 
 export const head: DocumentHead = {
--- src/routes/plugin@storyblok.ts
+++ src/routes/plugin@storyblok.ts
@@ -1,7 +1,42 @@
-import { storyblokInit, apiPlugin } from "@storyblok/js";
+import { $, useOnWindow, useSignal } from "@builder.io/qwik";
+import {
+  storyblokInit,
+  apiPlugin,
+  type ISbStoryData,
+  type StoryblokBridgeConfigV2,
+  loadStoryblokBridge,
+} from "@storyblok/js";
 
 export const { storyblokApi } = storyblokInit({
   accessToken: import.meta.env.PUBLIC_STORYBLOK_TOKEN,
   use: [apiPlugin],
   bridge: true,
 });
+
+export const useStoryblok = (
+  initialStory: ISbStoryData,
+  bridgeOptions: StoryblokBridgeConfigV2 = {}
+) => {
+  const story = useSignal(initialStory);
+
+  useOnWindow(
+    "load",
+    $(async () => {
+      await loadStoryblokBridge();
+      const { StoryblokBridge, location } = window;
+      const storyblokInstance = new StoryblokBridge(bridgeOptions);
+
+      storyblokInstance.on(["published", "change"], () => {
+        // reload page if save or publish is clicked
+        location.reload();
+      });
+
+      storyblokInstance.on("input", (event) => {
+        // Access currently changed but not yet saved content via:
+        story.value = event?.story as ISbStoryData;
+      });
+    })
+  );
+
+  return story;
+};
